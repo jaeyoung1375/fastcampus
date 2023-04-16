196969324399248a858c2ac08dd1dba2e105249e
jaeyoung1375 <79030661+jaeyoung1375@users.noreply.github.com>
2023-04-16 22:54:15 +0900

    ch4 생성

---
+++ ch4_boot/src/main/java/com/ch4/Ch4BootApplication.java
@@ -0,0 +1,13 @@
+package com.ch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch4BootApplication.class, args);
+	}
+
+}
---
+++ ch4_boot/src/test/java/com/ch4/Ch4BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
